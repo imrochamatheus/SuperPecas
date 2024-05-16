111d894243a35e0adcca42faafbd13db339576b1
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-16 15:05:54 -0300

    :sparkles: feat: adicionar componente layout para funções compartilhadas

---
+++ semana_3/src/app/pages/layout/layout.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+
+import { HeaderComponent } from "./../../shared/components/header/header.component";
+
+import { NavLink } from "./layout.interfaces";
+
+@Component({
+  selector: "app-layout",
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    MatListModule,
+    HeaderComponent,
+    RouterLinkActive,
+    MatSidenavModule,
+  ],
+  templateUrl: "./layout.component.html",
+  styleUrl: "./layout.component.less",
+})
+export class LayoutComponent {
+  public readonly navLinks: NavLink[] = [
+    { label: "Home", path: "/home" },
+    { label: "Carros", path: "cars" },
+    { label: "Peças", path: "parts" },
+  ];
+}
