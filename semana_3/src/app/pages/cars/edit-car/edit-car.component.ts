038c84aec6393bd7f0522520fa58d3308ed7f530
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-18 01:07:02 -0300

    :sparkles: feat: adicionar edição de carros

--- semana_3/src/app/pages/cars/edit-car/edit-car.component.ts
+++ semana_3/src/app/pages/cars/edit-car/edit-car.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, inject } from "@angular/core";
 import { Location } from "@angular/common";
-import { ActivatedRoute, ParamMap } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
 import {
   FormGroup,
   Validators,
@@ -16,8 +16,7 @@ import { MatButton } from "@angular/material/button";
 import { HeaderComponent } from "../../../shared/components/header/header.component";
 import { CarsService } from "../../../shared/services/cars.service";
 import { Car } from "../../../interfaces/cars.interfaces";
-
-type FormType<T> = Partial<Record<keyof T, FormControl>>;
+import { AbstractEdit } from "../../../shared/utils/abstract-edit/abstract-edit";
 
 @Component({
   selector: "app-edit-car",
@@ -27,16 +26,10 @@ type FormType<T> = Partial<Record<keyof T, FormControl>>;
   templateUrl: "./edit-car.component.html",
   styleUrl: "./edit-car.component.less",
 })
-export class EditCarComponent implements OnInit {
-  private readonly _carService: CarsService = inject(CarsService);
-  private readonly _route: ActivatedRoute = inject(ActivatedRoute);
-  private readonly _location: Location = inject(Location);
-  private readonly _messageService: MessageService = inject(MessageService);
+export class EditCarComponent extends AbstractEdit<Car> implements OnInit {
+  protected _data: Car = {} as Car;
 
-  private _carId: string | null = null;
-
-  public itemData: Car = {} as Car;
-
+  public override form: FormGroup;
   public formKeys: Record<keyof Car, keyof Car> = {
     id: "id",
     nomeModelo: "nomeModelo",
@@ -44,76 +37,66 @@ export class EditCarComponent implements OnInit {
     codigoUnico: "codigoUnico",
   };
 
-  public carForm: FormGroup = new FormGroup<FormType<Car>>({
-    nomeModelo: new FormControl("", [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(255),
-    ]),
-    fabricante: new FormControl("", [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(255),
-    ]),
-    codigoUnico: new FormControl("", [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(255),
-    ]),
-  });
-
   public get headerTitle(): string {
-    return `Editar o carro ${this._carId}`;
+    return `Editar o carro ${this.itemId}`;
   }
-
   public get formIsValid(): boolean {
-    return this.carForm.valid;
+    return this.form.valid;
   }
 
-  constructor() {}
+  constructor(
+    protected readonly _messageService: MessageService,
+    protected readonly _carService: CarsService,
+    protected readonly _route: ActivatedRoute,
+    protected readonly _location: Location
+  ) {
+    super();
 
-  private fillForm() {
-    this.carForm.setValue({
-      [this.formKeys.nomeModelo]: this.itemData.nomeModelo,
-      [this.formKeys.fabricante]: this.itemData.fabricante,
-      [this.formKeys.codigoUnico]: this.itemData.codigoUnico,
+    this.form = new FormGroup({
+      nomeModelo: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(255),
+      ]),
+      fabricante: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(255),
+      ]),
+      codigoUnico: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(255),
+      ]),
     });
   }
 
-  private fetchData(): void {
-    if (!this._carId) {
+  protected preloadFormValues(): void {
+    this.form.patchValue({
+      [this.formKeys.nomeModelo]: this._data.nomeModelo,
+      [this.formKeys.fabricante]: this._data.fabricante,
+      [this.formKeys.codigoUnico]: this._data.codigoUnico,
+    });
+  }
+
+  protected fetchData(): void {
+    if (!this.itemId) {
       return;
     }
 
     this._carService
-      .getCarById(this._carId)
+      .getCarById(this.itemId)
       .pipe(take(1))
       .subscribe((data) => {
-        this.itemData = data;
-        this.fillForm();
+        this._data = data;
+        this.preloadFormValues();
       });
   }
 
-  private showErrorToast(): void {
-    this._messageService.add({
-      severity: "error",
-      summary: "Error",
-      detail: "Erro ao atualizar registro",
-    });
-  }
-
-  private showSuccessToast(): void {
-    this._messageService.add({
-      severity: "success",
-      summary: "Suceddo!",
-      detail: "Atualizado com sucesso",
-    });
-  }
-
-  private editItem(): void {
+  protected editItem(): void {
     const payload: Car = {
-      ...this.carForm.value,
-      id: this._carId,
+      ...this.form.value,
+      id: this.itemId,
     };
 
     this._carService
@@ -126,27 +109,6 @@ export class EditCarComponent implements OnInit {
         },
         error: this.showErrorToast.bind(this),
       });
-  }
-
-  private initRouteListener(): void {
-    this._route.paramMap.subscribe((params: ParamMap) => {
-      this._carId = params.get("id");
-      this.fetchData();
-    });
-  }
-
-  public goBack(): void {
-    this._location.back();
-  }
-
-  public onClear(): void {
-    this.carForm.reset();
-  }
-
-  public onSubmit(): void {
-    if (this.carForm.valid) {
-      this.editItem();
-    }
   }
 
   public ngOnInit(): void {
