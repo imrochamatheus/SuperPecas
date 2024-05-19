7d89555efdf86758275e07537c90050e1673e355
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-19 10:53:48 -0300

    :sparkles: feat: adicionar serviços compartilhados em cars.components

--- semana_3/src/app/pages/cars/cars.component.ts
+++ semana_3/src/app/pages/cars/cars.component.ts
@@ -1,44 +1,15 @@
-import { Component, OnInit, inject } from "@angular/core";
-import {
-  Router,
-  RouterOutlet,
-  NavigationEnd,
-  ActivatedRoute,
-} from "@angular/router";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
-import { Subject, filter, takeUntil } from "rxjs";
 import { CarsService } from "../../shared/services/cars.service";
+import { NavigationService } from "../../shared/services/navigation.service";
+import { NotificationService } from "../../shared/services/notification.service";
 
 @Component({
   selector: "app-cars",
   standalone: true,
   imports: [RouterOutlet],
-  providers: [CarsService],
+  providers: [CarsService, NavigationService, NotificationService],
   template: "<router-outlet/>",
 })
-export class CarsComponent implements OnInit {
-  private readonly _router = inject(Router);
-  private readonly _activatedRoute = inject(ActivatedRoute);
-  private readonly _destroy$ = new Subject<void>();
-
-  private listenRouterEvents(): void {
-    this._router.events
-      .pipe(
-        takeUntil(this._destroy$),
-        filter(
-          (value) => value instanceof NavigationEnd && value.url === "/cars"
-        )
-      )
-      .subscribe(this.redirectToList.bind(this));
-  }
-
-  private redirectToList(): void {
-    this._router.navigate(["list"], {
-      relativeTo: this._activatedRoute,
-    });
-  }
-
-  public ngOnInit(): void {
-    this.listenRouterEvents();
-  }
-}
+export class CarsComponent {}
