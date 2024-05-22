41c46956add4cb690069b0a7b44c8720462dd79c
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-22 09:39:40 -0300

    :recycle: refactor: Adicionar construturoes em carroDTO e topKFabricantesDTO

--- semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/model/DTO/CarroDTO.java
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/model/DTO/CarroDTO.java
@@ -1,16 +1,19 @@
 package br.com.masterclass.superpecas.model.DTO;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.internal.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CarroDTO {
     private Long id;
     private String modelo;
     private String codigo;
     private String fabricante;
-
-    public CarroDTO () {}
 }
 
--- semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/model/DTO/TopKFabricantesDTO.java
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/model/DTO/TopKFabricantesDTO.java
@@ -1,11 +1,15 @@
 package br.com.masterclass.superpecas.model.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TopKFabricantesDTO {
     private String fabricante;
     private Long quantidadeModelos;
 
-    public TopKFabricantesDTO() {}
 }
