8d20666586d7c44bba4661def384c07f35e78e56
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-22 09:40:09 -0300

    :sparkles: feat: adicionar classe para retorno de dados

---
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/model/DTO/ApiResponse.java
@@ -0,0 +1,17 @@
+package br.com.masterclass.superpecas.model.DTO;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(Include.NON_NULL)
+public class ApiResponse<T>{
+    private String msg;
+    private T data;
+}
