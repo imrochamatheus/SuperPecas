1870077ab14f052097c383c294c0a29d7fe028bb
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-22 14:04:19 -0300

    :sparkles: feat: implementar endpoint para top carros com mais peças

--- semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/model/DTO/TopKCarrosPecasDTO.java
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/model/DTO/TopKCarrosPecasDTO.java
@@ -1,11 +1,13 @@
 package br.com.masterclass.superpecas.model.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TopKCarrosPecasDTO {
     private String modeloCarro;
     private Long quantidadePecas;
-
-    public TopKCarrosPecasDTO () {}
 }
