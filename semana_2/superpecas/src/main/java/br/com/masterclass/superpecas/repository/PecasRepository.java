cc70e0ab768a08756682674e946cf3e6cc922404
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-22 09:41:23 -0300

    :sparkles: feat: adicionar método para buscar peças associadas a carros

--- semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/repository/PecasRepository.java
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/repository/PecasRepository.java
@@ -3,5 +3,8 @@ package br.com.masterclass.superpecas.repository;
 import br.com.masterclass.superpecas.model.Peca;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PecasRepository extends JpaRepository<Peca, Long> {
+    public List<Peca> findByCarroId(Long carroId);
 }
--- semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/service/PecasService.java
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/service/PecasService.java
@@ -1,8 +1,12 @@
 package br.com.masterclass.superpecas.service;
 
+import br.com.masterclass.superpecas.model.Peca;
 import br.com.masterclass.superpecas.repository.PecasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PecasService {
@@ -12,5 +16,9 @@ public class PecasService {
     @Autowired
     public PecasService (PecasRepository pecasRepository) {
         this.pecasRepository = pecasRepository;
+    }
+
+    public List<Peca> listarPecasPorCarroId (Long carroId) {
+        return this.pecasRepository.findByCarroId(carroId);
     }
 }
