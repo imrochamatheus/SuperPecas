feb66c4d53f7461ab681194b3af9327ab2c65e1c
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-22 14:03:16 -0300

    :bug: fic: corrigir filtragem de carros por termo

--- semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/repository/CarrosRepository.java
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/repository/CarrosRepository.java
@@ -13,12 +13,17 @@ import java.util.Optional;
 public interface CarrosRepository extends
         JpaRepository<Carro, Long>{
 
-    @Query(nativeQuery = true, value = "SELECT c FROM Carro c WHERE lower(c.modelo) LIKE lower(CONCAT('%', ?1, '%'))")
-    public Page<Carro> findByModelo(String termo, Pageable page);
+    @Query("SELECT c FROM Carro c WHERE lower(c.modelo) like lower(concat('%', ?1,'%'))" +
+            " OR lower(c.codigo) like lower(concat('%', ?1,'%'))" +
+            " OR lower(c.fabricante) like lower(concat('%', ?1,'%'))")
+    public Page<Carro> findByTerm(String termo, Pageable page);
 
     @Query("SELECT new br.com.masterclass.superpecas.model.DTO.TopKFabricantesDTO(c.fabricante, COUNT(c.modelo) as quantidadeModelo) FROM Carro" +
             " c GROUP BY fabricante ORDER BY quantidadeModelo DESC LIMIT 10")
     public List<TopKFabricantesDTO> getTopKFabricantes ();
+
+    @Query("Select c.fabricante FROM Carro as c GROUP BY c.fabricante")
+    public List<String> findAllFabricantes();
 
     public Optional<Carro> findByModeloOrCodigo(String modelo, String codigo);
 }
