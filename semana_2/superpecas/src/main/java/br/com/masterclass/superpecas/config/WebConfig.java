e9a5799a15f55754473d7627a60d47003f3325fa
imrochamatheus <im.rochamatheus@gmail.com>
2024-05-28 12:20:39 -0300

    :heavy_plus_sign: build: adicionar configurações de CORS

---
+++ semana_2/superpecas/src/main/java/br/com/masterclass/superpecas/config/WebConfig.java
@@ -0,0 +1,26 @@
+package br.com.masterclass.superpecas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class WebConfig
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
